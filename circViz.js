4c4df0494a8a0a5b6eac6876cd4fa966224a34cf
nikolasmurdock <nikolasmurdock@gmail.com>
2016-08-17 17:55:50 -0500

    add circles, avg axis and css
    add circles append. The x value corresponds to d.precio, the y value to
    d.valor and the r value to d.volumen.
    Add avg axis that change depending on the values of the csv. (d.precio
    and d.valor)
    add a main.css for styling. avg axis are drawn with a light alpha.

--- circViz.js
+++ circViz.js
@@ -4,7 +4,9 @@ var margin = {top: 20, right: 50, bottom: 0, left: 50},
 
 var padding = 20;
 
-var radio = 10;
+var totalPrecio = 0, 
+	totalValor  = 0,
+	numValores = 0;
 
 function circViz () {
 	var svg = d3.select("body")
@@ -13,39 +15,109 @@ function circViz () {
 				.attr("height",h + margin.top + margin.bottom)
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	/*Domains*/
-	var xDom = d3.extent(dataset, function(d){
-		return d[0];
-	})
-	var yDom = d3.extent(dataset,function(d)
-	{
-		return d[1];
-	})
 
 	/*Scales*/
 	var xScale = d3.scaleLinear()
-					.domain(xDom)
 					.range([0, w]);
 	var yScale = d3.scaleLinear()
-					.domain(yDom)
 					.range([h-padding,0]);
+	var vScale = d3.scaleLinear()
+					.range([0,30]);
+	var aScale = d3.scaleLinear()
+					.range([0,1]);
 
 	/*Axes*/
 	var xAxis = d3.axisBottom()
 					.scale(xScale)
-					.ticks(5);
+					.ticks(10);
 	var yAxis = d3.axisLeft()
 					.scale(yScale)
-					.ticks(5);
+					.ticks(10);
+	var avgYAxis = d3.axisLeft()
+					.scale(yScale)
+					.ticks(0);
+	var avgXAxis = d3.axisTop()
+					.scale(xScale)
+					.ticks(0);
 
-	d3.csv("table.csv", function(error, data) {
+	d3.csv("dataset.csv", function(error, data) {
+		var f = d3.format(".2");
 		if (error) throw error;
-		svg.appeng("g")
-			.attr("class", "xAxis")
-			.attr("transform", "translate(0, "+height+")")
+		/*Translate csv to usable integers or numbers*/
+		data.forEach(function(d) {
+			d.valor = +d.Valor;
+			d.precio = +d.Precio;
+			d.volumen = +d.Volumen;
+			totalPrecio += d.precio;
+			totalValor += d.valor;
+			numValores++;
+		})
+
+		var avgPrecio = totalPrecio / numValores;
+		var avgValor = totalValor / numValores;
+		/*Define the domains using the csv information*/
+		var xDom = d3.extent(data, function(d) {
+			return d.precio;
+		})
+		xScale.domain(xDom);
+		var yDom = d3.extent(data,function(d) {
+			return d.valor;
+		})
+		yScale.domain(yDom);
+		var vDom = d3.extent(data, function(d) {
+			return d.volumen;
+		})
+		vScale.domain(vDom);
+		aScale.domain(vDom);
+		/*Draw Circles*/
+		svg.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d) {
+			return xScale(d.precio);
+		})
+		.attr("cy", function(d) {
+			return yScale(d.valor);
+		})
+		.attr("r", function(d){
+			return vScale(d.volumen);
+		})
+		.attr("fill", function(d){
+			var rgbR = Math.round(Math.random() * 255);
+			//var rgbR = rgbR.toString(16);
+			var rgbG = Math.round(Math.random() * 255);
+			//var rgbG = rgbG.toString(16);
+			var rgbB = Math.round(Math.random() * 255);
+			//var rgbB = rgbG.toString(16);
+			var alph = +f(aScale(d.volumen));
+			return "rgba("+rgbR+","+rgbG+","+rgbB+","+alph+")";
+		});
+		svg.app
+		/*Draw Axes*/
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(0, "+(h-margin.top)+")")
 			.call(xAxis);
-		svg.append()
-	}
+		svg.append("g")
+			.attr("class", "axis")
+			.call(yAxis);
+		svg.append("g")
+			.attr("class", "avgAxis")
+			.attr("transform", "translate("+xScale(avgPrecio)+",0)")
+			.call(avgYAxis);
+		svg.append("g")
+			.attr("class", "avgAxis")
+			.attr("transform", "translate(0,"+yScale(avgValor)+")")
+			.call(avgXAxis);
+		d3.select("body")
+			.append("h1")
+			.text("Promedio Precio = "+ avgPrecio);
+		d3.select("body")
+			.append("h1")
+			.text("Promedio Valor = "+ avgValor);
+
+	})
 }
 
 $(document).ready(circViz);
