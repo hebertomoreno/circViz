61c24bb49598f2f893bd061df67e057054715f40
nikolasmurdock <nikolasmurdock@gmail.com>
2016-08-17 15:37:10 -0500

    Add domain, axes and scales.

--- circViz.js
+++ circViz.js
@@ -13,6 +13,39 @@ function circViz () {
 				.attr("height",h + margin.top + margin.bottom)
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	/*Domains*/
+	var xDom = d3.extent(dataset, function(d){
+		return d[0];
+	})
+	var yDom = d3.extent(dataset,function(d)
+	{
+		return d[1];
+	})
+
+	/*Scales*/
+	var xScale = d3.scaleLinear()
+					.domain(xDom)
+					.range([0, w]);
+	var yScale = d3.scaleLinear()
+					.domain(yDom)
+					.range([h-padding,0]);
+
+	/*Axes*/
+	var xAxis = d3.axisBottom()
+					.scale(xScale)
+					.ticks(5);
+	var yAxis = d3.axisLeft()
+					.scale(yScale)
+					.ticks(5);
+
+	d3.csv("table.csv", function(error, data) {
+		if (error) throw error;
+		svg.appeng("g")
+			.attr("class", "xAxis")
+			.attr("transform", "translate(0, "+height+")")
+			.call(xAxis);
+		svg.append()
+	}
 }
 
 $(document).ready(circViz);
